7cbb6b1ff69ff154c84ca2d5bf2f1e93c3d51b4f
raaaaz123 <rasheedmaliyekkal126@gmail.com>
2025-10-24 21:39:58 +0530

    Fix LangChain text splitter import - use langchain_text_splitters

--- app/services/qdrant_service.py
+++ app/services/qdrant_service.py
@@ -23,7 +23,7 @@ from qdrant_client.models import (
     FusionQuery,
     Fusion
 )
-from langchain_community.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 import PyPDF2
 import pdfplumber
